f32bcde79b3d1da7ad2a9b95a14a493b4ab35107
GemBox Dev Team <info@gemboxsoftware.com>
2019-11-07 05:57:21 +0100

    Updated GemBox.Pdf example projects.

--- GemBox.Pdf Examples/Convert/ConvertCs/Program.cs
+++ GemBox.Pdf Examples/Convert/ConvertCs/Program.cs
@@ -10,6 +10,6 @@ class Program
         using (PdfDocument document = PdfDocument.Load("Reading.pdf"))
             // In order to achieve the conversion of a loaded PDF file to image,
             // we just need to save a PdfDocument object to desired image format.
-            document.Save("Convert.pdf");
+            document.Save("Convert.png");
     }
 }
