fbe0c1fb418d0150d197acc1788d521607fa93fb
GemBox Dev Team <info@gemboxsoftware.com>
2021-07-29 10:01:54 +0200

    Added "OCR" example.

---
+++ C#/Advanced Features/Optical Character Recognition/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using GemBox.Pdf;
+using GemBox.Pdf.Content;
+using GemBox.Pdf.Ocr;
+
+class Program
+{
+    static void Main()
+    {
+        // If using Professional version, put your serial key below.
+        ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+        Example1();
+        Example2();
+    }
+
+    static void Example1()
+    {
+        using (PdfDocument document = OcrReader.Read("BookPage.jpg"))
+        {
+            var page = document.Pages[0];
+            var contentEnumerator = page.Content.Elements.All(page.Transform).GetEnumerator();
+
+            while (contentEnumerator.MoveNext())
+            {
+                if (contentEnumerator.Current.ElementType == PdfContentElementType.Text)
+                {
+                    var textElement = (PdfTextContent)contentEnumerator.Current;
+                    Console.WriteLine(textElement.ToString());
+                }
+            }
+        }
+    }
+
+    static void Example2()
+    {
+        // TesseractDataPath specifies the directory which contains language data.
+        // You can download the language data files from: https://www.gemboxsoftware.com/pdf/docs/ocr.html#language-data
+        var readOptions = new OcrReadOptions() { TesseractDataPath = "languagedata" };
+
+        // The language of the text.
+        readOptions.Languages.Add(OcrLanguages.German);
+
+        using (PdfDocument document = OcrReader.Read("GermanDocument.pdf", readOptions))
+        {
+            document.Save("GermanDocumentEditable.pdf");
+        }
+    }
+}
