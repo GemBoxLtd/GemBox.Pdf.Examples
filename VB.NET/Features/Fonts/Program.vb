144a73c9a4780dcf84f49a21ded151e37589e52a
Mario at GemBox <mario.zorica@gemboxsoftware.com>
2024-10-29 07:13:15 +0100

    Updated "Fonts" example.

--- VB.NET/Features/Fonts/Program.vb
+++ VB.NET/Features/Fonts/Program.vb
@@ -1,6 +1,5 @@
 Imports GemBox.Pdf
 Imports GemBox.Pdf.Content
-Imports System.Linq
 
 Module Program
 
@@ -18,21 +17,9 @@ Module Program
                 formattedText.FontSize = 48
                 formattedText.LineHeight = 72
 
-                ' Use the font family 'Almonte Snow' whose font file is located in the 'Resources' directory.
-                formattedText.FontFamily = New PdfFontFamily("Resources", "Almonte Snow")
-                formattedText.AppendLine("Hello World 1!")
-
-                ' Use the font family 'Almonte Woodgrain' whose font file is located in the 'Resources' location of the current assembly.
-                formattedText.FontFamily = New PdfFontFamily(Nothing, "Resources", "Almonte Woodgrain")
-                formattedText.AppendLine("Hello World 2!")
-
-                ' Another way to use the font family 'Almonte Snow' whose font file is located in the 'Resources' directory.
-                formattedText.FontFamily = PdfFonts.GetFontFamilies("Resources").First(Function(ff) ff.Name = "Almonte Snow")
-                formattedText.AppendLine("Hello World 3!")
-
-                ' Another way to use the font family 'Almonte Woodgrain' whose font file is located in the 'Resources' location of the current assembly.
-                formattedText.FontFamily = PdfFonts.GetFontFamilies(Nothing, "Resources").First(Function(ff) ff.Name = "Almonte Woodgrain")
-                formattedText.Append("Hello World 4!")
+                ' Use the font family 'Almonte Snow' whose font file is located in the 'MyFonts' directory.
+                formattedText.FontFamily = New PdfFontFamily("MyFonts", "Almonte Snow")
+                formattedText.AppendLine("Hello World!")
 
                 page.Content.DrawText(formattedText, New PdfPoint(100, 500))
             End Using
